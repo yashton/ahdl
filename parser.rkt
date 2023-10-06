5e518ff2979f01af1fe7c20bb559b4d56775ce48
Ashton Snelgrove <ashton.snelgrove@utah.edu>
2023-10-06 14:29:40 -0600

    Compiling parser and updated example

--- parser.rkt
+++ parser.rkt
@@ -1,22 +1,19 @@
 #lang brag
 
-script: type_alias | union_def | enum_def | module_def | encoding_def
-body:
-expr: num | variable
-
+hardware: type_alias | union_def | enum_def | module_def | encoding_def
 
 ;;;;;;;;;;;;;;;; Literals ;;;;;;;;;;;;;;;;
 ; Allowed to use a generic name
-size_def: "<" INTEGER? | NAME ">"
+size_def: "<" NUMBER? | NAME? ">"
 
 literal: splat_literal | binary_literal | octal_literal | decimal_literal | hex_literal | ascii_char_literal
 ; Splats can be used to fill in the remaining parts of literal, or a struct.
 splat_literal: ("'?" | "'1" | "'0") size_def?
 ; Literals should be clearly marked with their ordinality
-binary_literal: BINARY "'b" size_def?
-octal_literal: OCTAL "'o" size_def?
-decimal_literal: INTEGER "'d" size_def?
-hex_literal: HEX "'x" size_def?
+binary_literal: BINARY size_def?
+octal_literal: OCTAL size_def?
+decimal_literal: DECIMAL size_def?
+hex_literal: HEX size_def?
 ascii_char_literal: "\"" CHAR* "\"" size_def?
 
 ;;;;;;;;;;;;;;;; Types ;;;;;;;;;;;;;;;;
@@ -48,32 +45,37 @@ union_item: NAME "(" [union_item_member ("," union_item_member)*] ")"
 encoding_def: "encoding" NAME
 ;;;;;;;;;;;;;;;; Definitions ;;;;;;;;;;;;;;;;
 ; Should allow positive edge, negative edge, or level trigger?
-clk_def: "@" "{" ("pos" | "neg" | "lev")? NAME }
+clk_def: "@" "{" ("pos" | "neg" | "lev")? NAME "}"
 addr_def: "[" [NAME ("," NAME)*] "]"
-
+; TODO extend to relative parameters?
+template_def: "<" [NAME ("," NAME)*] ">"
 addr_bind: "@" "[" NAME "]"
 
-module_def: "module" NAME template? addr_def? clk_def? argument_list "=>" argument_list "{" module_body "}"
+module_def: "module" NAME template_def? addr_def? clk_def? argument_list "=>" argument_list "{" module_body "}"
 
 module_body: module_def | enum_def | union_def | type_alias |  bind_def
 argument_list: "(" [argument ("," argument)*] ")"
 argument: NAME addr_bind? "::" type
 
-bind_def: "bind" (NAME? "(" bindings ")" | bindings) "<=" (module_instance | expr)
-bindings: [left_binding (";" left_binding)*]
+bind_def: "bind" (NAME? "(" bind_args ")" | bind_args | "*") "<=" (module_instance | expr | bindset)
+bind_args: [left_binding (";" left_binding)*]
 
 ; Look into numeric parameters
 generic_defs: [type_generic ("," type_generic)]
-module_instance: NAME "<" generic_defs ">" "@" "{" [NAME ("," NAME)*] "}" "(" right_binding ")"
-module
+module_instance: NAME "<" generic_defs ">" "@" "{" [NAME ("," NAME)*] "}" "(" [right_binding ("," right_binding)*] ")"
+
+reference: NAME addr_ref? clk_ref?
+addr_ref: "[" expr "]"
+clk_ref: "@{" NAME (("+"|"-") INTEGER)"}"
+
 ;;;;;;;;;;;;;;;; Expressions ;;;;;;;;;;;;;;;;
-; Expressions have two types - value or bindings list.
-; bindings can't be used with operators, only bound
+; Expressions have two types - value expression or binding expressions.
+; bindings can't be used with operators
 ; when a top bind operation happens:
 ; if the RHS is a value, it is bound to the LHS (with destructure)
 ; if the RHS is a bindings, the bindings are applied to the outer scope
 
-expr: let_expr | if_expr | match_expr | reference_expr | binding_expr | binops | unops | slice_op | index_op | cast_op | type_hint
+expr: let_expr | if_expr | match_expr | binops | unops | slice_op | index_op | cast_op | type_hint | reference | literal
 
 binops: expr ("&" | "|" | "^" | "%" | "*" | "/" | "+" | "-" | "==" | "!=" | ">" | "<" | ">=" | "<=") expr
 unops: ("!" | "~" | "-") expr
@@ -88,20 +90,24 @@ type_hint: expr "::" type
 group_expr: "(" expr ")"
 tuple_expr: "(" [expr ("," expr)+] ")"
 
-left_binding: (NAME | destructure) "<-" expr
-right_binding: expr "->" reference
-let_expr: "let" (let_bind | "(" let_bind ")") "{" expr "}"
-let_bind: [left_binding ("," left_binding)*]
-destructure: "(" [destructure_item, ("," destructure_item)*] ")")
+let_clause: "let" (let_left_bind | "(" let_left_bind ")")
+let_expr: let_clause "{" expr "}"
+let_left_bind: [left_binding ("," left_binding)*]
+destructure: "(" [destructure_item, ("," destructure_item)*] ")"
 destructure_item: NAME | (NAME "::" type) | destructure | (NAME destructure) | literal
 
-reference: NAME addr_ref? clk_ref?
-addr_ref: "[" expr "]"
-clk_ref: "@{" NAME (("+"|"-") INTEGER)"}"
-
-if_expr: "if" expr "{" expr "}" ("else" "if" "{" expr "}")* "else" "{" expr "}"
-match_expr: "match" expr "{" [match_case (match_case)*] "}"
-match_case: match_clause "{" expr "}"
+if_expr: "if" expr "{" expr "}" ("else" "if" expr "{" expr "}")* "else" "{" expr "}"
+match_expr: "match" expr "{" [match_expr_case (match_expr_case)*] "}"
+match_expr_case: match_clause "{" expr "}"
 match_clause: destructure ("if" expr)?
-; The type of a bind_block is a binding list. Non-bindings are an error
-bind_block: [expr (";" expr)+]
+
+;;;;;;;;;;;;;;;; Binding ;;;;;;;;;;;;;;;;
+left_binding: (NAME | destructure) "<-" expr
+binding: right_binding | let_bind | if_bind | match_bind | bindset
+
+right_binding: expr "->" reference
+let_bind: let_clause "{" expr "}"
+if_bind: "if" expr "{" binding "}" ("else" "if" expr "{" binding "}")* "else" "{" binding "}"
+match_bind: "match" expr "{" [match_case_bind (match_case_bind)*] "}"
+match_case_bind: match_clause "{" binding "}"
+bindset: [binding (";" binding)*]
